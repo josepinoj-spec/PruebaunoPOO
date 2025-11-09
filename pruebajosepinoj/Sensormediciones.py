5993f0d5831dcac81b2638c7e36369383720559b
josepinojera <jose.pino50@inacapmail.cl>
2025-11-09 19:20:44 -0300

    ejecrcio Sensor de mediciones

---
+++ pruebajosepinoj/Sensormediciones.py
@@ -0,0 +1,32 @@
+#creaciond d la clase
+class Sensor:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        elf.mediciones = []
+    
+#Agregamos la medicion
+    def agregar_medicion(self,valor):
+        self.mediciones.append(valor)
+        print(f"Medición {valor} agregada al sensor '{self.nombre}'.")
+    
+#Para obtener la medicion promedio   
+    def obtener_promedio(self):
+        if not self.mediciones:
+            return "No hay mediciones registradas."
+        return sum(self.mediciones) / len(self.mediciones)
+
+#Obtener el valor maximo de la medicion
+    def obtener_maximo(self):
+        if not self.mediciones:
+            return "No hay mediciones registradas."
+        return max(self.mediciones)
+
+#Obtener el valor minimo de la medicion            
+    def obterner_minimo(self):
+        if not self.mediciones:
+            return "No hay mediciones registradas."
+        return min(self.mediciones)
+        
+#Mostrar el nombre de la medicion
+    def mostrar_nombre(self):       
+        return self.nombre
